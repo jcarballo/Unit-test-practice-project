04b6261f2503b989c1ebf832920d129a2a35f753
Janneth Carballo <DreamUp@JANNETH.(none)>
2012-01-17 15:06:57 -0400

    modificacion en el js

--- js/old-fuctions.js
+++ js/old-fuctions.js
@@ -1,32 +1,15 @@
-var compareno = new Array;
-var comparecount = 0;
+$(document).ready(function() {
+  jQuery.fn.selectCompare = function(){
+	  var values = [];
+	  var i = 0;
+	  this.each(function(){
+	     values[i++] = $(this).val();
+	  });
+		  return values;
+	  }
+	  
+	  var attr = $("input:checked").selectCompare();
+	  alert(attr); 
+});
 
-function selectCompare(comparePartID) {
-	var partItemID = comparePartID.id;
-	
-    if (comparePartID.checked) {
-        if (comparecount > 4) {
-            alert('Maximum of 4 comparison parts');
-        } else {
-            compareno[comparecount] = partItemID;
-            comparecount++;
-        }
-    } else {
-        for (var i = 0; i < comparecount; i++) {
-            if (compareno[i] == partItemID) {
-                for (var j = i; j < comparecount - 1; j++) {
-                    compareno[j] = compareno[j + 1];
-                }
-                comparecount--;
-                i = comparecount;
-            }
-        }
-    }
-    var cmstr = '';
-    for (var i = 0; i < comparecount; i++) {
-        if (i != 0) { cmstr += ';'; }
-        cmstr += compareno[i];
-    }
 
-    document.getElementById('txtCompareList').value = cmstr;
-}
